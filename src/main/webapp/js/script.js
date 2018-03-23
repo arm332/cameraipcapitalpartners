7c6cb7106b869e1ed6032aa93be788ed29030c40
André Moreira <arm332@gmail.com>
2018-03-23 15:35:59 -0300

    Change viewers interval to 3 mins

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -46,9 +46,9 @@ $(document).ready(function() {
 
     // Start auto camera loading
 
-    //$('.cam img').one('load', function() {
-    //	$trigger(this);
-    //});
+//    $('.cam img').one('load', function() {
+//    		$trigger(this);
+//    });
 
     // On camera partial loading (image corrupt or truncated) 
     
@@ -68,44 +68,44 @@ $(document).ready(function() {
     // Change camera loading interval
 
     $('.cam ul li a').on('click', function() {
-        $button = $(this).parents('.cam').find('.dropdown-toggle');
-        $button.html($(this).text() + ' <span class="caret"></span>');
-        $img = $(this).parents('.cam').find('img');
-        $img.data('interval', $(this).text());
-        $trigger($img);
+		$button = $(this).parents('.cam').find('.dropdown-toggle');
+		$button.html($(this).text() + ' <span class="caret"></span>');
+		$img = $(this).parents('.cam').find('img');
+		$img.data('interval', $(this).text());
+		$trigger($img);
     });
     
     // Change all cameras loading interval
 
     $('.pull-right ul li a').on('click', function() {
-        $('.cam ul li a:contains(' + $(this).text() + ')').each(function() {
-        	$(this).click();
-        });
+		$('.cam ul li a:contains(' + $(this).text() + ')').each(function() {
+			$(this).click();
+		});
     });
 
     // On show camera dialog event
 
     $('#cam-dialog').on('show.bs.modal', function(e) {
-        $header = $(e.relatedTarget).parents('.cam').find('h4');
-        $(this).find('.modal-title').text($header.text());
-        $camimg = $(e.relatedTarget).parents('.cam').find('img');
-    	$dlgimg = $(this).find('.modal-body').find('img');
-    	$dlgimg.data('interval', $camimg.data('interval'));
-        $dlgimg.data('url', $camimg.data('url'));
-    	$dlgimg.attr('src', $camimg.data('url'));
-    	$dlgimg.attr('alt', $header.text());
-        $trigger($dlgimg);
+		$header = $(e.relatedTarget).parents('.cam').find('h4');
+		$(this).find('.modal-title').text($header.text());
+		$camimg = $(e.relatedTarget).parents('.cam').find('img');
+		$dlgimg = $(this).find('.modal-body').find('img');
+		$dlgimg.data('interval', $camimg.data('interval'));
+		$dlgimg.data('url', $camimg.data('url'));
+		$dlgimg.attr('src', $camimg.data('url'));
+		$dlgimg.attr('alt', $header.text());
+		$trigger($dlgimg);
     });
 
     // On hide camera dialog event
 
     $('#cam-dialog').on('hide.bs.modal', function(e) {
-    	$dlgimg = $(this).find('.modal-body').find('img');
-    	$dlgimg.data('interval', '')
-        $dlgimg.data('url', '');
-    	$dlgimg.attr('src', '');
-        $id = $dlgimg.attr('id');
-        clearTimeout($timer[$id]);
+		$dlgimg = $(this).find('.modal-body').find('img');
+		$dlgimg.data('interval', '')
+		$dlgimg.data('url', '');
+		$dlgimg.attr('src', '');
+		$id = $dlgimg.attr('id');
+		clearTimeout($timer[$id]);
     });
 
     // Show camera tooltip
@@ -130,71 +130,71 @@ $(document).ready(function() {
     $('.cam').attr('draggable', 'true');
 
     $('[draggable=true]').on('drag', function(e) {
-    	//console.log(e);
+    		//console.log(e);
     });
     
     $('[draggable=true]').on('dragstart', function(e) {
-    	// store a ref. on the draggable elem
+    		// store a ref. on the draggable elem
         $dragged = $(e.currentTarget);
         $dropped = $dragged;
     });
     
     $('[draggable=true]').on('dragenter', function(e) {
-    	//console.log(e);
+		//console.log(e);
     });
     
     $('[draggable=true]').on('dragover', function(e) {
-    	// prevent default to allow drop
-    	e.preventDefault();
-    	
-    	if (!$dropped.is(e.currentTarget)) {
-    		$dropped.children().css('pointer-events', '');
-    		$dropped.css('background-color', '');
-    		$dropped = $(e.currentTarget);
-    		$dropped.css('background-color', '#eee');
-    		$dropped.children().css('pointer-events', 'none');
-    	}
+		// prevent default to allow drop
+		e.preventDefault();
+		
+		if (!$dropped.is(e.currentTarget)) {
+			$dropped.children().css('pointer-events', '');
+			$dropped.css('background-color', '');
+			$dropped = $(e.currentTarget);
+			$dropped.css('background-color', '#eee');
+			$dropped.children().css('pointer-events', 'none');
+		}
     });
     
     $('[draggable=true]').on('dragleave', function(e) {
-    	//console.log(e);
+		//console.log(e);
     });
     
     $('[draggable=true]').on('dragend', function(e) {
-    	$dragged = null;
+		$dragged = null;
     });
     
     $('[draggable=true]').on('drop', function(e) {
-    	// prevent default action (open as link for some elements)
+		// prevent default action (open as link for some elements)
         e.preventDefault();
         
         // move dragged elem to the selected drop target
-    	var dropIndex = $dropped.index();
-    	var dragIndex = $dragged.index();
+		var dropIndex = $dropped.index();
+		var dragIndex = $dragged.index();
+		
+		if (dropIndex > dragIndex) {
+			$dropped.after($dragged);
+		}
+		else if (dropIndex < dragIndex) {
+			$dropped.before($dragged);
+		}
+		else {
+			// dragging to itself
+		}
     	
-    	if (dropIndex > dragIndex) {
-    		$dropped.after($dragged);
-    	}
-    	else if (dropIndex < dragIndex) {
-    		$dropped.before($dragged);
-    	}
-    	else {
-    		// dragging to itself
-    	}
-    	
-    	//
+		//
 		$dropped.children().css('pointer-events', '');
 		$dropped.css('background-color', '#fff');
-    	$dropped = null;
+		$dropped = null;
+		
+		console.log("drop")
     	
-    	console.log("drop")
-    	
-    	// Save cumstom order cookie
+		// Save cumstom order cookie
         var order = [];
         $('.cam > img').each(function(img) {
         	console.log(this.id + ", " + this.alt);
             // Remove all but numbers from ID
-        	order.push(this.id.replace(/\D/g, ""));
+        		order.push(this.id.replace(/\D/g, ""));
         });
         //console.log(order);
         //console.log(document.cookie);
@@ -206,27 +206,27 @@ $(document).ready(function() {
     // Get current on-line viewers
     
     $viewers = function() {
-    	$.ajax({
-        	dataType: 'json',
-        	url: '/viewers' 
-        })
+	    	$.ajax({
+	        	dataType: 'json',
+	        	url: '/viewers' 
+	    })
         .done(function(data) {
-        	//console.log(data)
-        	$html = '<b>Usuários Ativos</b>';
-        	$.each(data, function(key, val) {
-        		$html += '<br/>' + key;
-        	});
-        	//$html += '<br/>' + new Date();
-    		$('#viewers').html($html);
-    		// Cameras has it's own refresh every 1 min
-    		if ($path[1] != 'camera') {
-    			setTimeout($viewers, 1000 * 60 * 1); // 1 min
-    		}
-    	})
-    	.fail(function() {
-    		console.log('ERROR: at viewers');
-    	});
-    };
+	        	//console.log(data)
+	        	$html = '<b>Usuários Ativos</b>';
+	        	$.each(data, function(key, val) {
+	        		$html += '<br/>' + key;
+	        	});
+	        	//$html += '<br/>' + new Date();
+	    		$('#viewers').html($html);
+	    		// Cameras has it's own refresh every 3 min
+	    		if ($path[1] != 'camera') {
+	    			setTimeout($viewers, 1000 * 60 * 3); // 3 min
+	    		}
+	    	})
+	    	.fail(function() {
+	    		console.log('ERROR: at viewers');
+	    	});
+	};
     
     // Avoid request when not logged in as we won't receive any data anyway
     
